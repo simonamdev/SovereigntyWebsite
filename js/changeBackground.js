0a5231f56ba5f3aa4b64a1d3dddb4795e864a73d
DracaDragon <JasmineSMoniz@gmail.com>
2017-07-20 21:01:59 -0700

    First Initialization of the entire site

--- js/changeBackground.js
+++ js/changeBackground.js
@@ -2,7 +2,7 @@
 
 function changeImg()
 {
-	var newImgNumber =Math.floor(Math.random()*3);
-	document.getElementById('background').src = 'communitybackgrounds/background'+newImgNumber+'.png';
+	var newImgNumber =Math.floor(Math.random()*2);
+	document.getElementById('background').src = '../communitybackgrounds/background'+newImgNumber+'.png';
 }
 window.onload=changeImg;
